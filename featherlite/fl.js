ffce97c6b908a7a87ec7d515e45045acc62a515e
Shel <gryzon@gmail.com>
2021-04-05 21:16:06 -0400

    REFACTOR: Made fl a self-contained object
    - also externalized tests

--- featherlite/fl.js
+++ featherlite/fl.js
@@ -1,70 +1,73 @@
 const {encode, decode} = require('gpt-3-encoder');
-const lterm = (s) => s.charAt(0).toLowerCase() + s.slice(1);
-const uterm = (s) => s.charAt(0).toUpperCase() + s.slice(1);
-const l_term = (s) => " " + lterm(s);
-const u_term = (s) => " " + uterm(s);
-const cmpTokens = (a,b) => { if ( a.length != b.length ) return false; for ( let i=0; i<a.length; i++ ){ if ( a[i].trim().toLowerCase() != b[i].trim().toLowerCase() ) return false; } return true; }
-const cmpTokensStart = (f,p) => { if ( f.length < p.length ) throw 'Terms out of order'; for ( let i=0; i<p.length; i++){ if (f[i].trim().toLowerCase() != p[i].trim().toLowerCase() ) return false; } return true; }
-const tokenizeRegEx = /('[\w\s]+'|\w+|[^\w^\s]+?)/g;
-const tokenizePhrase = (s) => {
-	const matches = [...s.matchAll(tokenizeRegEx)];
-	let idx = 0;
-	return matches.map(m => m[idx]);
+
+const fl = {
+	lterm: (s) => s.charAt(0).toLowerCase() + s.slice(1),
+	uterm: (s) => s.charAt(0).toUpperCase() + s.slice(1),
+	l_term: (s) => " " + fl.lterm(s),
+	u_term: (s) => " " + fl.uterm(s),
+	cmpTokens: (a,b) => { if ( a.length != b.length ) return false; for ( let i=0; i<a.length; i++ ){ if ( a[i].trim().toLowerCase() != b[i].trim().toLowerCase() ) return false; } return true; },
+	cmpStartTokens: (f,p) => { if ( f.length < p.length ) throw 'Terms out of order'; for ( let i=0; i<p.length; i++){ if (f[i].trim().toLowerCase() != p[i].trim().toLowerCase() ) return false; } return true; },
+	tokenizeRegEx: /('[\w\s]+'|\w+|[^\w^\s]+?)/g,
+	skipRegEx: /^'.*'/g,
+	upperRegEx: /^[A-Z]\w+/g,
+	lowerRegEx: /^[a-z]\w+/g,
+	symbolRegEx: /[\W]+/g,
+	tokenizeInput: (s) => [...s.matchAll(fl.tokenizeRegEx)].map(m => m[0]),
+	tokenize: (w) => encode(w).map( (e) => decode([e]) ),
+	smash: (input) => {
+		if (!input) throw 'Input missing'
+		let phrase = "";
+		phraseTokens = fl.tokenizeInput(input);
+		for (let pToken of phraseTokens) {
+			let term;
+			if ( pToken.match( fl.skipRegEx ) ) { // Unbreakable token
+				term = pToken;
+			} else if ( pToken.match( fl.upperRegEx ) ) { // keep input capitals
+				term = fl.tokenize(fl.u_term(pToken)).length < fl.tokenize(fl.uterm(pToken)).length ? fl.u_term(pToken) : pToken;
+			} else if ( pToken.match( fl.lowerRegEx ) ) { // prefer lower case
+				let term1 = fl.tokenize(fl.l_term(pToken)).length < fl.tokenize(fl.lterm(pToken)).length ? fl.l_term(pToken) : pToken;
+				let term2 = fl.tokenize(fl.u_term(pToken)).length < fl.tokenize(fl.uterm(pToken)).length ? fl.u_term(pToken) : fl.uterm(pToken);
+				term = fl.tokenize(term2).length < fl.tokenize(term1).length ? term2 : term1;
+			} else if ( pToken.match( fl.symbolRegEx ) ) { // symbols
+				term = fl.tokenize(" " + pToken).length < fl.tokenize(pToken).length ? " " + pToken : pToken;
+			} else {
+				term = pToken;
+			}
+			if (!phrase) {
+				phrase = term;
+			} else if ( term.match( fl.upperRegEx )  ) {
+				phrase += fl.cmpStartTokens(fl.tokenize(phrase+term),fl.tokenize(phrase)) ? term : fl.l_term(term);
+			} else if ( term.match( fl.lowerRegEx ) ) {
+				if ( fl.cmpStartTokens(fl.tokenize(phrase+term),fl.tokenize(phrase)) ) {
+					phrase += term;
+				} else if ( fl.cmpStartTokens(fl.tokenize(phrase+fl.uterm(term)),fl.tokenize(phrase)) ) {
+					phrase += fl. uterm(term);
+				} else {
+					phrase += fl.l_term(term);
+				}
+			} else {
+				phrase += term;
+			}
+		}
+		return phrase;
+	}
 }
-const tokenizeWord = (w) => encode(w).map( (e) => decode([e]) );
 
 // Testing functionality
-if ( lterm("Hello") !== "hello" ) throw 'lterm fail';
-if ( uterm("hello") !== "Hello" ) throw 'uterm fail';
-if ( l_term("hello") !== " hello" ) throw 'l_term fail';
-if ( u_term("hello") !== " Hello" ) throw 'u_term fail';
-if ( !cmpTokens(["1","2","3"],["1","2","3"]) ) throw 'cmpTokens fail matched';
-if ( cmpTokens(["1","2","3"],["1","2","4"]) ) throw 'cmpTokens fail unmatched';
-if ( !cmpTokensStart(["1","2","3"],["1","2"]) ) throw 'cmpTokensStart fail matched';
-if ( cmpTokensStart(["1","2","3"],["1","3"]) ) throw 'cmpTokensStart fail unmatched';
-if ( !cmpTokens ( tokenizePhrase("< 'do this' test >"),["<","'do this'","test",">"] ) ) throw 'tokenizePhrase fail';
-if ( !cmpTokens ( tokenizeWord('tokenize'),["token","ize"] ) ) throw 'tokenizeWord fail';
+if ( fl.lterm("Hello") !== "hello" ) throw 'lterm fail';
+if ( fl.uterm("hello") !== "Hello" ) throw 'uterm fail';
+if ( fl.l_term("hello") !== " hello" ) throw 'l_term fail';
+if ( fl.u_term("hello") !== " Hello" ) throw 'u_term fail';
+if ( !fl.cmpTokens(["1","2","3"],["1","2","3"]) ) throw 'cmpTokens fail matched';
+if ( fl.cmpTokens(["1","2","3"],["1","2","4"]) ) throw 'cmpTokens fail unmatched';
+if ( !fl.cmpStartTokens(["1","2","3"],["1","2"]) ) throw 'cmpStartTokens fail matched';
+if ( fl.cmpStartTokens(["1","2","3"],["1","3"]) ) throw 'cmpStartTokens fail unmatched';
+if ( !fl.cmpTokens ( fl.tokenizeInput("< 'do this' test >"),["<","'do this'","test",">"] ) ) throw 'tokenizeInput fail';
+if ( !fl.cmpTokens ( fl.tokenize('tokenize'),["token","ize"] ) ) throw 'tokenize fail';
 
-const minFl = (input) => {
-	if (!input) throw 'Input missing'
-	let phrase = "";
-	phraseTokens = tokenizePhrase(input);
-	for (let pToken of phraseTokens) {
-		let term;
-		if ( pToken.match( /^'.*'/g ) ) { // Unbreakable token
-			term = pToken;
-		} else if ( pToken.match( /^[A-Z]\w+/g ) ) { // Keep upper
-			term = tokenizeWord(u_term(pToken)).length < tokenizeWord(uterm(pToken)).length ? u_term(pToken) : pToken;
-		} else if ( pToken.match( /^[a-z]\w+/g ) ) { // prefer lower
-			let term1 = tokenizeWord(l_term(pToken)).length < tokenizeWord(lterm(pToken)).length ? l_term(pToken) : pToken;
-			let term2 = tokenizeWord(u_term(pToken)).length < tokenizeWord(uterm(pToken)).length ? u_term(pToken) : uterm(pToken);
-			term = tokenizeWord(term2).length < tokenizeWord(term1).length ? term2 : term1;
-		} else if ( pToken.match( /[\W]+/g) ) {
-			term = tokenizeWord(" " + pToken).length < tokenizeWord(pToken).length ? " " + pToken : pToken;
-		} else {
-			term = pToken;
-		}
-		if (!phrase) {
-			phrase = term;
-		} else if ( term.match( /^[A-Z]/g )  ) { // upper
-			phrase += cmpTokensStart(tokenizeWord(phrase+term),tokenizeWord(phrase)) ? term : l_term(term);
-		} else if ( term.match( /^[a-z]/g ) ) { // lower
-			if ( cmpTokensStart(tokenizeWord(phrase+term),tokenizeWord(phrase)) ) {
-				phrase += term;
-			} else if ( cmpTokensStart(tokenizeWord(phrase+uterm(term)),tokenizeWord(phrase)) ) {
-				phrase += uterm(term);
-			} else {
-				phrase += l_term(term);
-			}
-		} else {
-			phrase += term;
-		}
-	}
-	return phrase;
-}
-
+// Execution
 let input = process.argv.slice(2).join(' ');
-let output = minFl(input);
-let count = tokenizeWord(output).length;
+let output = fl.smash(input);
+let count = fl.tokenize(output).length;
 console.log(output);
 console.log(`Tokens: ${count}`)
