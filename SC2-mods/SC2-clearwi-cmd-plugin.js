56912c87df3bc0185b939be62f7ef9e74b0c2fae
Shel <gryzon@gmail.com>
2021-04-19 22:44:01 -0400

    FIX: Fixed MAJOR bug in SC2 /clearwi plugin

---
+++ SC2-mods/SC2-clearwi-cmd-plugin.js
@@ -0,0 +1,21 @@
+(function() {
+    var _commandHandler = SimpleContextPlugin.commandHandler;
+    SimpleContextPlugin.prototype.commandHandler = function(text) {
+        // do something additional
+        let match = SC_RE.INPUT_CMD.exec(text)
+        if (match) match = match.filter(v => !!v)
+        if (!match || match.length < 2) return text
+    
+        // Check if the command was valid
+        const cmd = match[1].toLowerCase()
+        if (cmd === "clearwi") {
+            console.log("Clearing world entries")
+            worldEntries.filter(e => e.id).forEach(e => removeWorldEntry(e.id));
+            return "";            
+        } else {
+            console.log("Invoking original handler")
+            _commandHandler(text);
+        }
+    };
+    console.log("commandHandler augmented");
+})();
